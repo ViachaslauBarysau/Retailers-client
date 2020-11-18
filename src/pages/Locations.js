c97a0da445be1d8f184416e94bdfa92e877a685f
Artem <azvisx@gmail.com>
2020-11-19 02:10:36 +0300

    add some logic to Location

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,14 +1,67 @@
 import LocationModal from '../modals/LocationModal';
-import React, { useState, useEffect } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import {Button, Form} from 'react-bootstrap';
 
 export default () => {
-    const [locationsData, setData] = useState({
+    const [locationsData, setLocationsData] = useState({
         isLoading: true,
         error: null,
         locations: [],
         displayModal: false,
+        checkedRecords: [],
+        isDataOutdated: false
     });
+
+    const [selectedLocations, setSelectedLocations] = useState({});
+
+    function handleChange(e) {
+        if (e.target.checked) {
+            let checkedRecords = locationsData.checkedRecords;
+            checkedRecords.push(e.target.value);
+            setLocationsData((prevState) => ({
+                    ...prevState,
+                    checkedRecords
+                })
+            );
+        } else {
+            let checkedRecords = locationsData.checkedRecords;
+            checkedRecords = locationsData.checkedRecords.filter(elem => elem !== e.target.value);
+
+            setLocationsData((prevState) => ({
+                    ...prevState,
+                    checkedRecords
+                })
+            );
+        }
+    }
+
+    function removeLocations(e) {
+        e.preventDefault();
+        let locationIdList = [];
+        e.target.locations.forEach(element => {
+            element.checked && locationIdList.push({id: element.value});
+        });
+        fetch('http://localhost:8080/locations', {
+            headers: {
+                'Authorization': localStorage.getItem("token"),
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify(
+                locationIdList
+            ),
+            method: "DELETE"
+        });
+        //TODO с бэка принять JSON в котором будет указано что мы не можем удалить конкретную позицию по причине.....
+        //TODO логика ниже работает только при успешном удалении всех .then .catch
+        setLocationsData(
+            (prevState) => ({
+                ...prevState,
+                checkedRecords: [],
+                isDataOutdated: !locationsData.isDataOutdated
+            })
+        );
+    }
 
     useEffect(() => {
         fetch('http://localhost:8080/locations', {
@@ -21,85 +74,79 @@ export default () => {
         })
             .then(res => res.json())
             .then(locations => {
-                setData((prevState) => ({
+                setLocationsData((prevState) => ({
                     ...prevState,
                     isLoading: false,
                     locations
                 }));
             })
             .catch(e => {
-                setData((prevState) => ({
+                setLocationsData((prevState) => ({
                     ...prevState,
                     isLoading: false,
                     error: e
                 }))
             })
-    }, [locationsData.displayModal]);
+    }, [locationsData.displayModal, locationsData.isDataOutdated]);
 
-    const { isLoading, error, locations, displayModal } = locationsData;
+    const {isLoading, error, locations, displayModal} = locationsData;
 
     return (
         <>
             {isLoading && 'Loading....'}
             {!isLoading && !error &&
-                <Form onSubmit={removeLocations}>
-                    {(locations.length != 0
-                        ? <table border="1" width="100%">
-                            <tr>
-                                <th></th>
-                                <th>Identifier</th>
-                                <th>Type</th>
-                                <th>Full address</th>
-                                <th>Available/Total capacity</th>
-                            </tr>
-                            {locations.map(location => <Locations location={location} key={location.id} />)}
-                        </table>
-                        : 'Empty list')}
-                    <Button onClick={() => setData((prevState) => ({
-                        ...prevState,
-                        displayModal: true
-                    }))}>
-                        Add location
-                        </Button>
-                    <Button type="submit">
-                        Remove location
-                        </Button>
-                </Form>
+            <Form onSubmit={removeLocations}>
+                {(locations.length !== 0
+                    ? <table border="1" width="100%">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>Identifier</th>
+                            <th>Type</th>
+                            <th>Full address</th>
+                            <th>Available/Total capacity</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {locations.map(location => <Location
+                            key={location.id}
+                            location={location}
+                            onChange={handleChange}
+                        />)}
+                        </tbody>
+                    </table>
+                    : 'Empty list')}
+                <Button onClick={() => setLocationsData((prevState) => ({
+                    ...prevState,
+                    displayModal: true
+                }))}>
+                    Add location
+                </Button>
+                <Button type="submit" disabled={locationsData.checkedRecords.length === 0}>
+                    Remove location
+                </Button>
+            </Form>
             }
             {!isLoading && error && 'Error happens'}
-            {displayModal && <LocationModal onClick={() => setData((prevState) => ({ ...prevState, displayModal: false }))} />}
+            {displayModal &&
+            <LocationModal onClick={() => setLocationsData((prevState) => ({...prevState, displayModal: false}))}/>}
         </>
     );
 }
 
-function removeLocations(e) {
-    e.preventDefault();
-    let locationIdList = [];
-    e.target.locations.forEach(element => {
-        element.checked && locationIdList.push({ id: element.value });
-    });
-    fetch('http://localhost:8080/locations', {
-        headers: {
-            'Authorization': localStorage.getItem("token"),
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify(
-            locationIdList
-        ),
-        method: "DELETE"
-    });
-}
 
-function Locations({ location }) {
+function Location(props) {
+
     return (
-        <tr id={location.id}>
-            <td><input type="checkbox" value={location.id} name={"locations"} /></td>
-            <td>{location.identifier}</td>
-            <td>{location.locationType}</td>
-            <td>{location.address.state.name}, {location.address.city}, {location.address.firstAddressLine}</td>
-            <td>{location.availableCapacity}/{location.totalCapacity}</td>
+        <tr id={props.location.id}>
+            <td><input type="checkbox"
+                       value={props.location.id}
+                       name={"locations"}
+                       onChange={props.onChange}/></td>
+            <td>{props.location.identifier}</td>
+            <td>{props.location.locationType}</td>
+            <td>{props.location.address.state.name}, {props.location.address.city}, {props.location.address.firstAddressLine}</td>
+            <td>{props.location.availableCapacity}/{props.location.totalCapacity}</td>
         </tr>
-
     )
 }
